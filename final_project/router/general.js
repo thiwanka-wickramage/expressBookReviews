9470315ba7bb8ec0da20683b43cfe8f91e3fa288
Thiwanka Wickramage <thiwanka.wickramage@activate.sg>
2025-05-02 16:20:11 +0800

    updated with Async-Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,33 +20,88 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  return res.status(200).json(books);
+public_users.get("/", async function (req, res) {
+  try {
+    const getBooks = () => {
+      return new Promise((resolve, reject) => {
+        resolve(books);
+      });
+    };
+
+    const bookList = await getBooks();
+    return res.status(200).json(bookList);
+  } catch (err) {
+    return res.status(500).json({ message: "Error fetching books" });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", (req, res) => {
+public_users.get("/isbn/:isbn", async (req, res) => {
   const { isbn } = req.params;
-  const book = books[isbn];
 
-  if (!book) {
+  const getBookByISBN = () => {
+    return new Promise((resolve, reject) => {
+      const book = books[isbn];
+      if (book) {
+        resolve(book);
+      } else {
+        reject("Book not found");
+      }
+    });
+  };
+
+  try {
+    const book = await getBookByISBN();
+    return res.status(200).json(book);
+  } catch (error) {
     return res.status(404).json({ message: "Could not find the book for the given ISBN." });
   }
-  return res.status(200).json(book);
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async (req, res) => {
   const { author } = req.params;
-  const bookResult = Object.values(books).filter((book) => book.author === author);
-  return res.status(200).json(bookResult);
+
+  const getBooksByAuthor = () => {
+    return new Promise((resolve, reject) => {
+      const result = Object.values(books).filter((book) => book.author === author);
+      if (result.length > 0) {
+        resolve(result);
+      } else {
+        reject("No books found for the given author.");
+      }
+    });
+  };
+
+  try {
+    const booksByAuthor = await getBooksByAuthor();
+    return res.status(200).json(booksByAuthor);
+  } catch (error) {
+    return res.status(404).json({ message: error });
+  }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async (req, res) => {
   const { title } = req.params;
-  const bookResult = Object.values(books).filter((book) => book.title === title);
-  return res.status(200).json(bookResult);
+
+  const getBooksByTitle = () => {
+    return new Promise((resolve, reject) => {
+      const result = Object.values(books).filter((book) => book.title === title);
+      if (result.length > 0) {
+        resolve(result);
+      } else {
+        reject("No books found with the given title.");
+      }
+    });
+  };
+
+  try {
+    const booksByTitle = await getBooksByTitle();
+    return res.status(200).json(booksByTitle);
+  } catch (error) {
+    return res.status(404).json({ message: error });
+  }
 });
 
 //  Get book review
